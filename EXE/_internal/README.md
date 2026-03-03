149a7f58d5a68fd98e0a34a2c82ae6a0d047964e
CYGear <Gear70704@gmail.com>
2026-03-02 22:09:00 -0500

    EXE Commit
    Sending

---
+++ EXE/_internal/README.md
@@ -0,0 +1,7 @@
+Hey, I made this quick task CLI task manager to practice some python :)
+
+If you want to use this just install python 3.13 and run main.py
+
+You can add sections and tasks inside those sections. They all save after you leave the program so you can leave them there.
+
+The main purpose of this program is to bundle all your tasks and organize them to get 
---
+++ EXE/_internal/task.py
@@ -0,0 +1,71 @@
+import json
+
+class TaskManager:
+    def __init__(self):
+        try:
+            # if data.json exists load it
+            with open("data.json", "r") as file:
+                try:
+                    # data.json already formatted as a dictionary load it
+                    self.data = json.load(file)
+                except:
+                    # data.json is empty create an empty dictionary
+                    self.data = {}
+        except:
+            # if data.json doesn't exist create it
+            with open("data.json", "w") as file:
+                json.dump({}, file)
+            with open("data.json", "r") as file:
+                self.data = json.load(file)
+                
+                
+    def Add_Section(self, name):
+        if name not in self.data:
+            self.data[name] = []
+            self.save()
+            
+    def Del_Section(self, name):
+        try:
+            del self.data[name]
+            self.save()
+        except:
+            print(f"{name} not a valid section!")
+    
+    def Add_Task(self, section, name, time):
+        try:
+            task = {"name": name, "time": time}
+            self.data[section].append(task)
+            self.save()
+        except:
+            print("Invalid Section!")
+
+    def Del_Task(self, section, name):
+        if section in self.data:
+            for i,task in enumerate(self.data[section]):
+                if task["name"] == name:
+                    del self.data[section][i]
+                    self.save()
+                    break
+    
+    def  Show_Tasks(self):
+        print(self.data)
+    
+    def Calc_Time(self, section):
+        time = 0
+        try:
+            for task in self.data[section]:
+                time += int(task["time"])
+            return time
+        except:
+            return time
+    
+    def Calc_Full_Time(self):
+        time = 0
+        for section in self.data:
+           for task in self.data[section]:
+                time += int(task["time"])
+        return time
+                  
+    def save(self):
+        with open("data.json", "w") as file:
+            json.dump(self.data, file, indent=4)
